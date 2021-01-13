205487fd8ed7f81f16c7b46fa5cc2742337dc682
welove-t <tlove.tky@gmail.com>
2021-01-14 06:09:39 +0900

    fix(index):サンプル部分を修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,73 +24,13 @@ const IndexPage = () => (
           <h2 className="text-xl　m-0 p-0 font-bold text-left">サンプル</h2>
           <div className="bg-white shadow overflow-hidden sm:rounded-lg">
             <div className="px-4 py-5 sm:px-6">
-              <h3 className="text-lg leading-6 font-medium text-gray-900">
-                Book Title
+              <h3 className="text-lg leading-6 font-medium text-gray-900 h-96">
+                イメージ（画像添付予定）
               </h3>
             </div>
-            <div className="border-t border-gray-200">
-              <dl>
-                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">
-                    Book Detail
-                  </dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    Book Detail
-                  </dd>
-                </div>
-                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">
-                  Book Detail
-                  </dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  Book Detail
-                  </dd>
-                </div>
-                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">
-                  Book Detail
-                  </dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  Book Detail
-                  </dd>
-                </div>
-                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">
-                  Book Detail
-                  </dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  Book Detail
-                  </dd>
-                </div>
-                <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">
-                  Book Detail
-                  </dt>
-                  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  Book Detail
-                  </dd>
-                </div>
-                <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                  <dt className="text-sm font-medium text-gray-500">
-                  Book Detail
-                  </dt>
-                </div>
-              </dl>
-            </div>
           </div>
-
         </div>
-
-        {/* <h1 className="text-3xl m-0 p-0 font-bold text-center">サンプル</h1> */}
-          
-
       </section>
-
-    {/* <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p> */}
     </div>
   </Layout>
 )
