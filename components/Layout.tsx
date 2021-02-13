622bed039793ab51edc76f1660f42e6affe9c289
welove-t <tlove.tky@gmail.com>
2021-02-13 17:01:26 +0900

    fix(seach&heddaer):検索欄をヘッダーに配置

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,46 +1,93 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode, useState } from "react";
+import Link from "next/link";
+import Head from "next/head";
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header className="fixed top-0 -inset-x-0 bg-blue-600">
-      <div className="container-sm mx-auto px-2">
-        <div className="items-center flex justify-between p-4 border-b-1">
-          <a className="text-white text-base">
-          LOGO
+const Layout = ({ children, title = "This is the default title" }: Props) => {
+  const [searchBarVisible, setSearchBarVisible] = useState(false);
+  const onClickSearchBarVisible = () => {
+    setSearchBarVisible(!searchBarVisible);
+  };
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header className="fixed top-0 -inset-x-0 bg-blue-600 p-2 flex justify-between items-center md:space-x-2">
+        <a className="text-white text-base">LOGO</a>
+        <div className="items-center hidden w-full sm:flex">
+          <input
+            type="text"
+            className="ml-3 md:w-11/12 whitespace-nowrap px-2 py-2 border border-white rounded-l-md shadow-sm text-sm font-medium outline-none"
+            placeholder="本のタイトルを入力"
+          />
+          <a
+            href="#"
+            className="inline-flex items-center justify-center px-2 py-2 border border-white rounded-r-md shadow-sm text-sm font-medium text-white hover:bg-blue-500"
+          >
+            <img src="/images/search.svg" alt="" className="w-5 rounded-md" />
           </a>
-          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <a href="#" className="ml-3 whitespace-nowrap inline-flex items-center justify-center px-2 py-2 border border-white rounded-md shadow-sm text-sm font-medium text-white hover:bg-blue-500">
-              本を検索する
-            </a>
-            <a href="#" className="ml-3 whitespace-nowrap inline-flex items-center justify-center px-2 py-2 border border-white rounded-md shadow-sm text-sm font-medium text-white hover:bg-blue-500">
-              ログイン
-            </a>
-          </nav>
         </div>
-      </div>
-    </header>
-    <main>{children}</main>
-    <footer className="pt-10 pb-8 bg-gray-50">
-      <div className="flex flex-wrap justify-center space-x-4">
-        <a href="#">利用規約</a>
-        <a href="#">プライバシーポリシー</a>
-        <a href="#">お問合せ</a>
-      </div>
-      <p className="text-center m-4">©️E-LOVE</p>
-    </footer>
-  </div>
-)
+        <div className="flex items-center">
+          <a
+            href="#"
+            className="inline-flex items-center justify-center px-2 py-2 border border-white rounded-md shadow-sm text-sm font-medium text-white hover:bg-blue-500 sm:hidden"
+            onClick={onClickSearchBarVisible}
+          >
+            <img src="/images/search.svg" alt="" className="w-5" />
+          </a>
+          <a
+            href="#"
+            className="ml-2 whitespace-nowrap inline-flex items-center justify-center px-2 py-2 border border-white rounded-md shadow-sm text-sm font-medium text-white hover:bg-blue-500"
+          >
+            ログイン
+          </a>
+        </div>
+      </header>
 
-export default Layout
+      {searchBarVisible && (
+        <div className="fixed top-0 -inset-x-0 bg-blue-600 p-2 flex justify-between items-center sm:hidden">
+          <div className="flex items-center w-full">
+            <a
+              href="#"
+              className="inline-flex items-center justify-center px-1 py-1 shadow-sm text-sm font-nomal text-white hover:bg-blue-500"
+              onClick={onClickSearchBarVisible}
+            >
+              <img src="/images/arrow-left.svg" alt="" className="w-5" />
+            </a>
+
+            <input
+              type="text"
+              className="ml-1 w-9/12 whitespace-nowrap px-2 py-2 border border-white rounded-l-md shadow-sm text-sm font-medium outline-none"
+              placeholder="本のタイトルを入力"
+            />
+            <a
+              href="#"
+              className="inline-flex items-center justify-center px-2 py-2 border border-white rounded-r-md shadow-sm text-sm font-medium text-white hover:bg-blue-500"
+            >
+              <img src="/images/search.svg" alt="" className="w-5 rounded-md" />
+            </a>
+          </div>
+        </div>
+      )}
+
+      <main>{children}</main>
+      <footer className="pt-10 pb-8 bg-gray-50">
+        <div className="flex flex-wrap justify-center space-x-4">
+          <a href="#">利用規約</a>
+          <a href="#">プライバシーポリシー</a>
+          <a href="#">お問合せ</a>
+        </div>
+        <p className="text-center m-4">©️E-LOVE</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
--- pages/search.tsx
+++ pages/search.tsx
@@ -4,19 +4,8 @@ export default function Search() {
   return (
     <Layout>
       <div className="py-7 px-4">
-        <form className="flex flex-grow w-full">
-          <input
-            type="text"
-            className="w-full bg-white rounded border border-gray-300 focus:border-blue-600 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-            placeholder="本のタイトルを入力してください"
-          />
-          <button className="w-20 text-white bg-blue-600 border-0 py-2 px-4 focus:outline-none hover:bg-blue-600 rounded text-md">
-            検索
-          </button>
-        </form>
-        　
-        <div className="pt-8">
-          <p>検索結果</p>
+        <div className="pt-10">
+          <h1>検索結果</h1>
           <div className="grid grid-cols-2 gap-4 bg-grey-light py-8 w-full @screen sm:grid-cols-4">
             <div className="bg-white rounded w-1/8 shadow hover:shadow-md duration-4">
               <div className="p-2 text-grey-darker text-justify flex flex-col">
