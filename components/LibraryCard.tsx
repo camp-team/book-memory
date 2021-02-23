a2cb0aae3e646e8e36b0ecb8e22ea294041c4ba9
welove-t <tlove.tky@gmail.com>
2021-02-23 11:01:28 +0900

    feature(Library):モアアバウトの実装

--- components/LibraryCard.tsx
+++ components/LibraryCard.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import LibraryMemory from '../components/LibraryMemory';
+import MoreVar from '../components/MoreVer';
 
 type Props = {
   bid: number;
@@ -29,35 +30,37 @@ const textMemories = [
   },
 ];
 
-const LibraryCard = ({ bid, imgUrl, title }: Props) => (
-  <div className='p-1 flex flex-col bg-white rounded w-full shadow hover:shadow-md duration-4'>
-    <div className='relative p-1 flex'>
-      <img
-        src={imgUrl}
-        alt='Some image'
-        className='w-8 flex self-center shadow-lg'
-      />
-      <p className='p-1 font-bold text-sm'>{title}</p>
-      <a href='#' className='ml-2 flex-shrink-0'>
+const LibraryCard = ({ bid, imgUrl, title }: Props) => {
+  const onBookDelete = () => {};
+  return (
+    <div className='p-1 flex flex-col bg-white rounded w-full shadow hover:shadow-md duration-4'>
+      <div className='group relative p-1 flex'>
         <img
-          src='/images/dots.svg'
-          alt=''
-          className='w-4 rounded-md absolute top-0 right-0 opacity-30'
+          src={imgUrl}
+          alt='Some image'
+          className='w-8 flex self-center shadow-lg'
         />
-      </a>
+        <p className='p-1 font-bold text-sm'>{title}</p>
+        <div className='ml-2 absolute top-0 right-0'>
+          <MoreVar
+            onFunctions={[onBookDelete]}
+            children={['ライブラリから削除']}
+          />
+        </div>
+      </div>
+      <form className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
+        <ul className='border border-gray-200 rounded-md divide-y divide-gray-200'>
+          {textMemories.map(
+            (textMemory) =>
+              bid === textMemory.id && (
+                <div key={textMemory.id}>
+                  <LibraryMemory texts={textMemory.texts} />
+                </div>
+              )
+          )}
+        </ul>
+      </form>
     </div>
-    <form className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
-      <ul className='border border-gray-200 rounded-md divide-y divide-gray-200'>
-        {textMemories.map(
-          (textMemory) =>
-            bid === textMemory.id && (
-              <div key={textMemory.id}>
-                <LibraryMemory texts={textMemory.texts} />
-              </div>
-            )
-        )}
-      </ul>
-    </form>
-  </div>
-);
+  );
+};
 export default LibraryCard;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {},
   },
   variants: {
+    opacity: ['group-hover'],
     extend: {},
   },
   plugins: [],
