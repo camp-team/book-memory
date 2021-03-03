90d7985d736904bedae2a0335c17b27bca20be84
welove-t <tlove.tky@gmail.com>
2021-03-03 19:45:03 +0900

    fix(Layout):singlequote

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,15 +1,15 @@
-import React, { ReactNode, useState, useContext } from "react";
-import Link from "next/link";
-import Head from "next/head";
-import { AuthContext } from "../pages/_app";
-import LoginDialogButton from "../components/LoginDialogButton";
+import React, { ReactNode, useState, useContext } from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
+import { AuthContext } from '../pages/_app';
+import LoginDialogButton from '../components/LoginDialogButton';
 
 type Props = {
   children?: ReactNode;
   title?: string;
 };
 
-const Layout = ({ children, title = "ブックメモリー" }: Props) => {
+const Layout = ({ children, title = 'ブックメモリー' }: Props) => {
   const currentUser = useContext(AuthContext).currentUser;
   const [searchBarVisible, setSearchBarVisible] = useState(false);
 
@@ -21,50 +21,50 @@ const Layout = ({ children, title = "ブックメモリー" }: Props) => {
     <div>
       <Head>
         <title>{title}</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
-      <header className="fixed top-0 -inset-x-0 bg-blue-600 p-2 px-4 flex justify-between items-center md:space-x-2">
-        <a className="text-white text-base">LOGO</a>
-        <div className="ml-4 items-center hidden w-1/2 sm:flex">
+      <header className='fixed top-0 -inset-x-0 bg-blue-600 p-2 px-4 flex justify-between items-center md:space-x-2'>
+        <a className='text-white text-base'>LOGO</a>
+        <div className='ml-4 items-center hidden w-1/2 sm:flex'>
           <input
-            type="text"
-            className="md:w-11/12 whitespace-nowrap px-2 py-2 border border-white rounded-l-md shadow-sm text-sm font-medium outline-none"
-            placeholder="本のタイトルを入力"
+            type='text'
+            className='md:w-11/12 whitespace-nowrap px-2 py-2 border border-white rounded-l-md shadow-sm text-sm font-medium outline-none'
+            placeholder='本のタイトルを入力'
           />
           <a
-            href="#"
-            className="inline-flex px-2 py-2 border border-white rounded-r-md shadow-sm text-sm font-medium text-white hover:bg-blue-500"
+            href='#'
+            className='inline-flex px-2 py-2 border border-white rounded-r-md shadow-sm text-sm font-medium text-white hover:bg-blue-500'
           >
-            <img src="/images/search.svg" alt="" className="w-5 rounded-md" />
+            <img src='/images/search.svg' alt='' className='w-5 rounded-md' />
           </a>
         </div>
-        <div className="flex items-center">
+        <div className='flex items-center'>
           <a
-            href="#"
-            className="inline-flex items-center justify-center px-2 py-2 border border-white rounded-md shadow-sm text-sm font-medium text-white hover:bg-blue-500 sm:hidden"
+            href='#'
+            className='inline-flex items-center justify-center px-2 py-2 border border-white rounded-md shadow-sm text-sm font-medium text-white hover:bg-blue-500 sm:hidden'
             onClick={onClickSearchBarVisible}
           >
-            <img src="/images/search.svg" alt="" className="w-5" />
+            <img src='/images/search.svg' alt='' className='w-5' />
           </a>
           {!currentUser ? (
             currentUser === null ? (
               //未ログインのパターン（ログインボタン表示）
-              <LoginDialogButton stylecss="ml-4 p-2 border border-white rounded-md shadow-sm text-sm font-medium text-white hover:bg-blue-500">
+              <LoginDialogButton stylecss='ml-4 p-2 border border-white rounded-md shadow-sm text-sm font-medium text-white hover:bg-blue-500'>
                 ログイン
               </LoginDialogButton>
             ) : (
               //ログイン情報取得中のパターン（つまりcurrentUserがundefiendの状態）
               //※Googleアイコン描写時に検索欄が動かないようにするためのダミー
-              <div className="ml-4 rounded-full h-9 w-9"></div>
+              <div className='ml-4 rounded-full h-9 w-9'></div>
             )
           ) : (
             //ログイン情報取得済のパターン（アイコン表示）
-            <a href="#">
+            <a href='#'>
               <img
-                src={currentUser?.photoURL || "https://picsum.photos/100/100"}
-                alt="Some image"
-                className="ml-4 rounded-full h-9 w-9 flex items-center justify-center"
+                src={currentUser?.photoURL || 'https://picsum.photos/100/100'}
+                alt='Some image'
+                className='ml-4 rounded-full h-9 w-9 flex items-center justify-center'
               />
             </a>
           )}
@@ -72,39 +72,39 @@ const Layout = ({ children, title = "ブックメモリー" }: Props) => {
       </header>
 
       {searchBarVisible && (
-        <div className="fixed top-0 -inset-x-0 bg-blue-600 p-2 flex justify-between items-center sm:hidden">
-          <div className="flex items-center w-full">
+        <div className='fixed top-0 -inset-x-0 bg-blue-600 p-2 flex justify-between items-center sm:hidden'>
+          <div className='flex items-center w-full'>
             <a
-              href="#"
-              className="inline-flex items-center justify-center px-1 py-1 shadow-sm text-sm font-nomal text-white hover:bg-blue-500"
+              href='#'
+              className='inline-flex items-center justify-center px-1 py-1 shadow-sm text-sm font-nomal text-white hover:bg-blue-500'
               onClick={onClickSearchBarVisible}
             >
-              <img src="/images/arrow-left.svg" alt="" className="w-5" />
+              <img src='/images/arrow-left.svg' alt='' className='w-5' />
             </a>
 
             <input
-              type="text"
-              className="ml-1 w-9/12 whitespace-nowrap px-2 py-2 border border-white rounded-l-md shadow-sm text-sm font-medium outline-none"
-              placeholder="本のタイトルを入力"
+              type='text'
+              className='ml-1 w-9/12 whitespace-nowrap px-2 py-2 border border-white rounded-l-md shadow-sm text-sm font-medium outline-none'
+              placeholder='本のタイトルを入力'
             />
             <a
-              href="#"
-              className="inline-flex items-center justify-center px-2 py-2 border border-white rounded-r-md shadow-sm text-sm font-medium text-white hover:bg-blue-500"
+              href='#'
+              className='inline-flex items-center justify-center px-2 py-2 border border-white rounded-r-md shadow-sm text-sm font-medium text-white hover:bg-blue-500'
             >
-              <img src="/images/search.svg" alt="" className="w-5 rounded-md" />
+              <img src='/images/search.svg' alt='' className='w-5 rounded-md' />
             </a>
           </div>
         </div>
       )}
 
       <main>{children}</main>
-      <footer className="pt-10 pb-8 bg-gray-50">
-        <div className="flex flex-wrap justify-center space-x-4">
-          <a href="#">利用規約</a>
-          <a href="#">プライバシーポリシー</a>
-          <a href="#">お問合せ</a>
+      <footer className='pt-10 pb-8 bg-gray-50'>
+        <div className='flex flex-wrap justify-center space-x-4'>
+          <a href='#'>利用規約</a>
+          <a href='#'>プライバシーポリシー</a>
+          <a href='#'>お問合せ</a>
         </div>
-        <p className="text-center m-4">©️E-LOVE</p>
+        <p className='text-center m-4'>©️E-LOVE</p>
       </footer>
     </div>
   );
