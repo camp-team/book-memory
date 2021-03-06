032a002e359c0fc854a73a693521ef5847a82f6b
welove-t <tlove.tky@gmail.com>
2021-03-06 13:07:35 +0900

    feature(Library):本記録追加機能（画面上のみ）

--- components/LibraryCard.tsx
+++ components/LibraryCard.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'react';
 import LibraryMemory from '../components/LibraryMemory';
 import MoreVar from '../components/MoreVer';
 
@@ -6,41 +6,38 @@ type Props = {
   bid: number;
   imgUrl: string;
   title: string;
+  texts: string[];
 };
 
-const textMemories = [
-  {
-    id: 0,
-    texts: [],
-  },
-  {
-    id: 1,
-    texts: [
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Quam quidem laborum, quae impedit laboriosam dolore nemorecusandae, iusto blanditiis soluta omnis hic magni sit? Expedita!',
-      'Quo officia perspiciatis accusamus aliquam repellendus optio vel ipsum cupiditate tenetur hic voluptate fugit laudantium quod aliquid itaque quasi enim, dicta ducimus.',
-    ],
-  },
-  {
-    id: 2,
-    texts: [
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit.Quam quidem laborum, quae impedit laboriosam dolore nemorecusandae, iusto blanditiis soluta omnis hic magni sit? Expedita!',
-      'Quo officia perspiciatis accusamus aliquam repellendus optio vel ipsum cupiditate tenetur hic voluptate fugit laudantium quod aliquid itaque quasi enim, dicta ducimus.',
-      'laudantium quod aliquid itaque quasi enim, dicta ducimus.',
-    ],
-  },
-];
-
-const LibraryCard = ({ bid, imgUrl, title }: Props) => {
+const LibraryCard = ({ bid, imgUrl, title, texts }: Props) => {
   const onBookDelete = () => {};
+  const [input, setInput] = useState('');
+  const [memories, setMemory] = useState(texts);
+  const onChangeInput: any = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+    console.log(typeof event.target);
+  };
+  //記録追加関数
+  const onClickMemoryAdd: VoidFunction = () => {
+    if (input === '') return;
+    const newText = [...memories, input];
+    setMemory(newText);
+    setInput('');
+  };
+
+  useEffect(() => {
+    console.log(memories);
+  });
+
   return (
-    <div className='p-1 flex flex-col bg-white rounded w-full shadow hover:shadow-md duration-4'>
-      <div className='group relative p-1 flex'>
+    <div className='p-1 flex flex-col bg-blue-100 rounded w-full shadow hover:shadow-md duration-4'>
+      <div className='group relative p-1 flex flex-col'>
         <img
           src={imgUrl}
           alt='Some image'
-          className='w-8 flex self-center shadow-lg'
+          className='w-16 flex self-center shadow-lg'
         />
-        <p className='p-1 font-bold text-sm'>{title}</p>
+        <p className='p-2 font-semibold text-sm text-center'>{title}</p>
         <div className='ml-2 absolute top-0 right-0'>
           <MoreVar
             onFunctions={[onBookDelete]}
@@ -50,14 +47,12 @@ const LibraryCard = ({ bid, imgUrl, title }: Props) => {
       </div>
       <form className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
         <ul className='border border-gray-200 rounded-md divide-y divide-gray-200'>
-          {textMemories.map(
-            (textMemory) =>
-              bid === textMemory.id && (
-                <div key={textMemory.id}>
-                  <LibraryMemory texts={textMemory.texts} />
-                </div>
-              )
-          )}
+          <LibraryMemory
+            input={input}
+            onChange={onChangeInput}
+            onClick={onClickMemoryAdd}
+            memories={memories}
+          />
         </ul>
       </form>
     </div>
--- components/LibraryMemory.tsx
+++ components/LibraryMemory.tsx
@@ -1,24 +1,29 @@
-import * as React from 'react';
+import React from 'react';
 import MoreVar from '../components/MoreVer';
+import IconButton from '@material-ui/core/IconButton';
+import CheckOutlinedIcon from '@material-ui/icons/CheckOutlined';
 type Props = {
-  texts?: string[];
+  input: string;
+  onChange: VoidFunction;
+  onClick: VoidFunction;
+  memories?: string[];
 };
 
-const LibraryMemory = ({ texts }: Props) => {
+const LibraryMemory = ({ input, onChange, onClick, memories }: Props) => {
   const onMemoryEdit = () => {};
   const onMemoryDelete = () => {};
 
   return (
     <>
-      {texts &&
-        texts.map((text) => (
+      {memories &&
+        memories.map((memory, index) => (
           <li
-            key={text}
-            className='group p-1 items-center text-sm cursor-pointer'
+            key={index}
+            className='group px-4 items-center text-sm cursor-pointer'
           >
-            <div className='flex p-1 border-b-2 bg-white border-gray-100 rounded-md'>
-              <p className='ml-1 rounded-md flex-1 bg-white whitespace-pre-line break-all'>
-                {text}
+            <div className='flex p-1 bg-white border-gray-100 rounded-md'>
+              <p className='pl-3 rounded-md flex-1 bg-white whitespace-pre-line break-all'>
+                {memory}
               </p>
 
               <MoreVar
@@ -28,17 +33,24 @@ const LibraryMemory = ({ texts }: Props) => {
             </div>
           </li>
         ))}
-      <li className='p-2 items-center text-sm'>
-        <div className='flex relative'>
-          <textarea
-            wrap='hard'
-            placeholder='入力する'
-            className='ml-2 mr-6 flex-1 resize-none focus:outline-none'
-          ></textarea>
+      <li className='mx-4 items-center text-sm flex relative rounded-md bg-white'>
+        <textarea
+          wrap='hard'
+          value={input}
+          onChange={onChange}
+          placeholder='入力する'
+          className='ml-3 mr-7 p-1 rounded-md flex-1 resize-none focus:outline-none'
+        ></textarea>
 
-          <a href='#' className='flex-shrink-0 absolute bottom-0 right-0'>
-            <img src='/images/check.svg' alt='' className='w-4' />
-          </a>
+        <div className='ml-2 absolute bottom-0 right-0 '>
+          <IconButton
+            className='focus:outline-none'
+            size='small'
+            style={{ color: 'green' }}
+            onClick={onClick}
+          >
+            <CheckOutlinedIcon fontSize='small' className='text-sm' />
+          </IconButton>
         </div>
       </li>
     </>
