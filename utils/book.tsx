d1aa6feb82661d28d0d7397e4325d0b3c4699ea1
welove-t <tlove.tky@gmail.com>
2021-03-14 16:25:21 +0900

    fix(book):コメント削除

--- utils/book.tsx
+++ utils/book.tsx
@@ -1,7 +1,6 @@
 import 'firebase/auth';
 import 'firebase/firestore';
 import firebase from 'firebase/app';
-import { useEffect, useState } from 'react';
 
 export const addBook = (id: string, name: string, imgUrl: string) => {
   firebase.firestore().doc(`books/${id}`).set({
@@ -10,19 +9,3 @@ export const addBook = (id: string, name: string, imgUrl: string) => {
     memories: [],
   });
 };
-
-//？？？？Bookの状態は持っておく必要ない？？？？
-// export const useBook = (id: string) => {
-//   const [memories, setMemory] = useState<any>();
-
-//   useEffect(() => {
-//     firebase
-//       .firestore()
-//       .doc(`books/${id}`)
-//       .onSnapshot((snap) => {
-//         setMemory(snap.data()?.comments || []);
-//       });
-//   }, []);
-
-//   return memories;
-// };
