f51081a35571c9527783f4cfedb8ccc692d45bf9
welove-t <tlove.tky@gmail.com>
2021-03-31 08:02:18 +0900

    feature(search):検索処理(GoogleBooksAPI)

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,18 +1,37 @@
 import Layout from '../components/Layout';
 import BookCard from '../components/BookCard';
-import { books } from '../utils/search-books';
+import { useRouter } from 'next/router';
+import { searchBooks } from '../utils/search-books';
+import { useEffect, useState } from 'react';
+import { Book } from '../interfaces/Book';
 
 export default function Search() {
+  const router = useRouter();
+  //検索ヘッダの本のタイトルを取得
+  const booktitle: any = router.query.booktitle; //検索結果の保管ステート
+  const [bookList, setBookList] = useState<Book[]>([]);
+  //本検索処理　ヘッダが検索される都度処理発生
+  useEffect(() => {
+    booktitle &&
+      searchBooks(booktitle).then((books: Book[] | undefined) => {
+        books && setBookList(books);
+      });
+  }, [booktitle]);
   return (
     <Layout>
-      <div className='container mx-auto pt-20 text-center'>
-        　<p>検索結果</p>
-        <div className='grid grid-cols-2 gap-2 py-8 md:grid-cols-4 md:gap-4'>
-          {books.map((book: any, idx) => (
-            <div key={idx}>
-              <BookCard bid={book.id} imgUrl={book.imgUrl} title={book.title} />
-            </div>
-          ))}
+      <div className='pt-20 text-center bg-blue-100'>
+        　<p className='font-bold text-2xl'>検索結果</p>
+        <div className='container mx-auto grid grid-cols-2 gap-2 py-8 md:grid-cols-4 md:gap-4'>
+          {bookList &&
+            bookList.map((book: Book, idx) => (
+              <div key={idx}>
+                <BookCard
+                  bid={book.bid}
+                  imgUrl={book.imgUrl}
+                  title={book.title}
+                />
+              </div>
+            ))}
         </div>
       </div>
     </Layout>
--- utils/search-books.ts
+++ utils/search-books.ts
@@ -1,32 +1,20 @@
-export const books = [
-  {
-    id: 0,
-    imgUrl:
-      'https://images-na.ssl-images-amazon.com/images/I/51srrwLATIL._SX390_BO1,204,203,200_.jpg',
-    title: '1冊ですべて身につくHTML & CSSとWebデザイン入門講座',
-  },
-  {
-    id: 1,
-    imgUrl: 'https://m.media-amazon.com/images/I/41i89jqMyPL.jpg',
-    title: 'なるほどデザイン',
-  },
-  {
-    id: 2,
-    imgUrl:
-      'https://images-na.ssl-images-amazon.com/images/I/51ekcrVqOSL._SX350_BO1,204,203,200_.jpg',
-    title: 'キタミ式イラストIT塾 基本情報技術者 令和03年',
-  },
+import { Book } from '../interfaces/Book';
 
-  {
-    id: 3,
-    imgUrl:
-      'https://images-na.ssl-images-amazon.com/images/I/51AvRbVdAsL._SX394_BO1,204,203,200_.jpg',
-    title:
-      '改訂新版JavaScript本格入門 ~モダンスタイルによる基礎から現場での応用までaaaaaaaaaaaaaaaaaaaaaaaaaa',
-  },
-  {
-    id: 4,
-    imgUrl: 'https://m.media-amazon.com/images/I/51E4g6djCbL.jpg',
-    title: '働き方の哲学 360度の視点で仕事を考える',
-  },
-];
+export const searchBooks = async (bookTitle: string) => {
+  if (!bookTitle) return;
+  const res = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`
+  );
+  if (!res) return;
+  const bookJson = await res.json();
+  const bookData: Book[] = await bookJson.items.map((data: any) => {
+    const item = data.volumeInfo;
+    const book: Book = {
+      bid: data.id,
+      title: item.title,
+      imgUrl: item.imageLinks ? item.imageLinks.smallThumbnail : '',
+    };
+    return book;
+  });
+  return bookData;
+};
