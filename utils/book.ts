6016706e60e31de5f55fa6382102da0c1f615442
welove-t <tlove.tky@gmail.com>
2021-04-24 11:15:07 +0900

    fix(library):ライブラリ表示のソート対応

--- utils/book.ts
+++ utils/book.ts
@@ -2,13 +2,16 @@ import 'firebase/auth';
 import 'firebase/firestore';
 import firebase from 'firebase/app';
 import { fuego } from '../utils/firebase';
+import { nowDate } from '../utils/now-date';
 
 export const addBook = (bid: string, name: string, imgUrl: string) => {
   const currentUser = fuego.auth().currentUser;
+  const createdat = nowDate();
   currentUser &&
     firebase.firestore().doc(`users/${currentUser.uid}/books/${bid}`).set({
       name: name,
       imgUrl: imgUrl,
+      createdat: createdat,
       memories: [],
     });
 };
