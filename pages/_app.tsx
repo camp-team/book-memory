5645dfa0a55f46cd35e2dfa90798da6bfb799465
welove-t <tlove.tky@gmail.com>
2021-05-01 08:42:10 +0900

    fix(_app)：不要ロジック削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,12 @@
 import Head from 'next/head';
-import { createContext, useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { fuego } from '../utils/firebase';
 import 'tailwindcss/tailwind.css';
-import { User } from 'firebase';
 import { FuegoProvider } from '@nandorojo/swr-firestore';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
-type AuthContextProps = {
-  currentUser: User | null | undefined;
-};
-
-export const AuthContext = createContext<AuthContextProps>({
-  currentUser: null,
-});
-
 const MyApp = ({ Component, pageProps }: any) => {
-  const [currentUser, setCurrentUser] = useState<User | null | undefined>(
-    undefined
-  );
-
   useEffect(() => {
     AOS.init({
       easing: 'ease-out-cubic',
@@ -29,12 +16,6 @@ const MyApp = ({ Component, pageProps }: any) => {
     });
   }, []);
 
-  useEffect(() => {
-    fuego.auth().onAuthStateChanged((user) => {
-      setCurrentUser(user);
-      currentUser || console.log(); //deploy回避用
-    });
-  });
   return (
     <>
       <Head>
