2e46d3a40bec9e221611156058b7404a07b2da73
welove-t <tlove.tky@gmail.com>
2021-05-03 10:43:57 +0900

    fix(login):Googleログインアイコンの日本語化

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
+const { merge } = require('webpack-merge');
 module.exports = {
   images: {
     domains: ['images-na.ssl-images-amazon.com', 'books.google.com'],
+  },
+  distDir: './.next',
+  webpack: (config) => {
+    return merge(config, {
+      resolve: {
+        alias: {
+          firebaseui: 'firebaseui-ja',
+        },
+      },
+    });
   },
 };
