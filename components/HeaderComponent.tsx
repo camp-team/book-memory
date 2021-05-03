0651d14567ce4ce86fc417086a263e89c7b26f24
welove-t <tlove.tky@gmail.com>
2021-05-03 10:44:54 +0900

    fix(head):検索欄のブランクトリム＆OGP調整

--- components/HeaderComponent.tsx
+++ components/HeaderComponent.tsx
@@ -12,6 +12,7 @@ import { useRouter } from 'next/router';
 import { fuego } from '@nandorojo/swr-firestore';
 import { NextSeo } from 'next-seo';
 import { User } from 'firebase';
+import Image from 'next/image';
 
 type Props = {
   children?: ReactNode;
@@ -31,7 +32,7 @@ export const Header = ({ title = 'BookMemory' }: Props) => {
 
   //検索結果画面へ
   const searchBook = (input: string) => {
-    if (input === '') return;
+    if (input.trim() === '') return;
     router.push({
       pathname: '/search',
       query: { booktitle: input },
@@ -53,10 +54,26 @@ export const Header = ({ title = 'BookMemory' }: Props) => {
     <div>
       <NextSeo
         title={title}
-        description="読書記録ツール"
+        description="超シンプルな読書記録ツール"
+        canonical="https://book-memory.com"
         openGraph={{
           url: `https://book-memory.com`,
-          images: [{ url: '/images/book-memory-ogp.jpg', alt: 'BookMemory' }],
+          title: 'BookMemory',
+          description: '超シンプルな読書記録ツール',
+          images: [
+            {
+              url: '/images/book-memory-ogp1.jpg',
+              width: 800,
+              height: 600,
+              alt: 'BookMemory',
+            },
+            {
+              url: '/images/book-memory-ogp2.jpg',
+              width: 800,
+              height: 600,
+              alt: 'BookMemory',
+            },
+          ],
         }}
         twitter={{
           handle: '@welove_tk',
@@ -66,8 +83,10 @@ export const Header = ({ title = 'BookMemory' }: Props) => {
       />
       <header className="fixed top-0 -inset-x-0 bg-blue-600 p-2 px-4 flex justify-between items-center md:space-x-2 z-30">
         <Link href="/">
-          <img
+          <Image
             src="/images/logo_BookMemory.png"
+            width={145}
+            height={22}
             alt=""
             className="w-36 cursor-pointer"
           />
@@ -90,7 +109,13 @@ export const Header = ({ title = 'BookMemory' }: Props) => {
             className="inline-flex px-2 py-2 border border-white rounded-r-md shadow-sm text-sm font-medium text-white hover:bg-blue-500"
             aria-label="検索"
           >
-            <img src="/images/search.svg" alt="" className="w-5 rounded-md" />
+            <Image
+              src="/images/search.svg"
+              width={30}
+              height={20}
+              alt=""
+              className="w-5 rounded-md"
+            />
           </a>
         </form>
         <div className="flex items-center">
@@ -100,7 +125,13 @@ export const Header = ({ title = 'BookMemory' }: Props) => {
             onClick={() => setSearchBarVisible(!searchBarVisible)}
             aria-label="検索欄表示"
           >
-            <img src="/images/search.svg" alt="" className="w-5" />
+            <Image
+              src="/images/search.svg"
+              width={25}
+              height={20}
+              alt=""
+              className="w-5"
+            />
           </a>
           {!user ? (
             user === null ? (
@@ -126,9 +157,14 @@ export const Header = ({ title = 'BookMemory' }: Props) => {
               className="inline-flex items-center justify-center px-1 py-1 shadow-sm text-sm font-nomal text-white hover:bg-blue-500"
               onClick={() => setSearchBarVisible(!searchBarVisible)}
             >
-              <img src="/images/arrow-left.svg" alt="" className="w-5" />
+              <Image
+                src="/images/arrow-left.svg"
+                width={25}
+                height={20}
+                alt=""
+                className="w-5"
+              />
             </a>
-
             <input
               type="text"
               className="ml-1 w-9/12 whitespace-nowrap px-2 py-2 border border-white rounded-l-md shadow-sm text-sm font-medium outline-none"
@@ -137,6 +173,7 @@ export const Header = ({ title = 'BookMemory' }: Props) => {
               onChange={(event: ChangeEvent<HTMLInputElement>) => {
                 setSearchInput(event.target.value);
               }}
+              onKeyDown={(event) => onKeyDownSeach(event, searchInput)}
             />
             <a
               href="#"
@@ -146,7 +183,13 @@ export const Header = ({ title = 'BookMemory' }: Props) => {
               className="inline-flex items-center justify-center px-2 py-2 border border-white rounded-r-md shadow-sm text-sm font-medium text-white hover:bg-blue-500"
               aria-label="検索"
             >
-              <img src="/images/search.svg" alt="" className="w-5 rounded-md" />
+              <Image
+                src="/images/search.svg"
+                width={25}
+                height={20}
+                alt=""
+                className="w-5 rounded-md"
+              />
             </a>
           </div>
         </div>
