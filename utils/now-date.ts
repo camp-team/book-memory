f2758cdccc5c7c10a174aa39ce165469009ac45c
welove-t <tlove.tky@gmail.com>
2021-05-03 17:41:04 +0900

    fix(search):fetch周りのリファクタ

--- utils/now-date.ts
+++
@@ -1,6 +0,0 @@
-import { format } from 'date-fns';
-
-export const nowDate = () => {
-  const now = new Date();
-  return format(now, 'yyyy-MM-dd HH:mm:ss');
-};
--- utils/search-books.ts
+++ utils/search-books.ts
@@ -1,13 +1,15 @@
 import { GoogleBook } from '../interfaces/Book';
 
-export const searchBooks = async (bookTitle: string | string[]) => {
-  if (!bookTitle) return;
+export const searchBooks = async (
+  bookTitle: string | string[]
+): Promise<GoogleBook[] | null> => {
+  if (!bookTitle) return null;
   const res = await fetch(
     `https://www.googleapis.com/books/v1/volumes?q=${bookTitle}`
   );
-  if (!res) return;
+  if (!res) return null;
   const bookJson = await res.json();
-  const bookData: GoogleBook[] = await bookJson.items.map((data: any) => {
+  const bookData: GoogleBook[] = bookJson.items.map((data: any) => {
     const item = data.volumeInfo;
     const book: GoogleBook = {
       bid: data.id,
